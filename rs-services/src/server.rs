4744d99323c3aed99d5427cebe02dea2d46daf0d
suwandre <suwandresukijat@gmail.com>
2024-06-14 00:07:36 +0200

    chore: updated dockerfiles to include slimmer runtime images; init rust and its dockerfile

---
+++ rs-services/src/server.rs
@@ -0,0 +1,19 @@
+use salvo::prelude::*;
+
+/// test function to check if Salvo is running
+#[handler]
+async fn run_salvo() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main()]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let router = Router::new()
+        .push(Router::new().path("").get(run_salvo));
+
+    let acceptor = TcpListener::new("0.0.0.0:3002").bind().await;
+    Server::new(acceptor).serve(router).await;
+}
+
