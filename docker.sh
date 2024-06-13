ac71ad660fc167269a32c595afb3925765682a83
suwandre <suwandresukijat@gmail.com>
2024-06-13 23:31:42 +0200

    chore: build and run both prod and dev instances in docker.sh

--- docker.sh
+++ docker.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 show_usage() {
-  echo "Usage: $0 {build-prod|up-prod|build-dev|up-dev|down-prod|down-dev|build-and-up-prod|build-and-up-dev}"
+  echo "Usage: $0"
   echo "Commands:"
-  echo "  build-prod       Build the production Docker images"
-  echo "  up-prod          Start the production Docker containers"
-  echo "  build-dev        Build the development Docker images"
-  echo "  up-dev           Start the development Docker containers"
-  echo "  down-prod        Stop and remove the production Docker containers"
-  echo "  down-dev         Stop and remove the development Docker containers"
-  echo "  build-and-up-prod  Build and start the production Docker images"
-  echo "  build-and-up-dev  Build and start the development Docker images"
+  echo "  build-prod            Build the production Docker images"
+  echo "  up-prod               Start the production Docker containers"
+  echo "  build-dev             Build the development Docker images"
+  echo "  up-dev                Start the development Docker containers"
+  echo "  down-prod             Stop and remove the production Docker containers"
+  echo "  down-dev              Stop and remove the development Docker containers"
+  echo "  build-and-up-prod     Build and start the production Docker images"
+  echo "  build-and-up-dev      Build and start the development Docker images"
+  echo "  build-and-up-prod-dev Build and start the production and development Docker images"
 }
 
 # call `show_usage()` if 0 or more than 1 arguments are provided
@@ -24,39 +25,44 @@ COMMAND=$1
 
 # run compose commands based on the command
 case $COMMAND in
-  build-prod)
-    COMPOSE_FILES="-f docker-compose.yml"
-    docker-compose $COMPOSE_FILES build
-    ;;
-  up-prod)
-    COMPOSE_FILES="-f docker-compose.yml"
-    docker-compose $COMPOSE_FILES up
-    ;;
-  build-dev)
-    COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
-    docker-compose $COMPOSE_FILES build
-    ;;
-  up-dev)
-    COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
-    docker-compose $COMPOSE_FILES up
-    ;;
-  down-prod)
-    COMPOSE_FILES="-f docker-compose.yml"
-    docker-compose $COMPOSE_FILES down
-    ;;
-  down-dev)
-    COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
-    docker-compose $COMPOSE_FILES down
-    ;;
-  build-and-up-prod)
-    COMPOSE_FILES="-f docker-compose.yml"
-    docker-compose $COMPOSE_FILES up --build
-    ;;
-  build-and-up-dev)
-    COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
-    docker-compose $COMPOSE_FILES run --rm ts-services npm run build
-    docker-compose $COMPOSE_FILES up --build
-    ;;
+    build-prod)
+        COMPOSE_FILES="-f docker-compose.yml"
+        docker-compose $COMPOSE_FILES build
+        ;;
+    up-prod)
+        COMPOSE_FILES="-f docker-compose.yml"
+        docker-compose $COMPOSE_FILES up
+        ;;
+    build-dev)
+        COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
+        docker-compose $COMPOSE_FILES build
+        ;;
+    up-dev)
+        COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
+        docker-compose $COMPOSE_FILES up
+        ;;
+    down-prod)
+        COMPOSE_FILES="-f docker-compose.yml"
+        docker-compose $COMPOSE_FILES down
+        ;;
+    down-dev)
+        COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
+        docker-compose $COMPOSE_FILES down
+        ;;
+    build-and-up-prod)
+        COMPOSE_FILES="-f docker-compose.yml"
+        docker-compose $COMPOSE_FILES up --build
+        ;;
+    build-and-up-dev)
+        COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
+        docker-compose $COMPOSE_FILES run --rm ts-services npm run build
+        docker-compose $COMPOSE_FILES up --build
+        ;;
+    build-and-up-prod-dev)
+        COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
+        docker-compose $COMPOSE_FILES run --rm ts-services npm run build
+        docker-compose $COMPOSE_FILES up --build
+        ;;
   *)
     show_usage
     exit 1
