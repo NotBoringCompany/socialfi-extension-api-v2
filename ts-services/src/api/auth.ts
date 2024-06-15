ad5c4a4afb4c1c168b333568b3580d98ff195094
suwandre <suwandresukijat@gmail.com>
2024-06-15 14:38:50 +0200

    feat: APIResponse and APIResponseStatus

---
+++ ts-services/src/api/auth.ts
@@ -0,0 +1,6 @@
+import { APIResponse } from '../utils/api';
+import { ExtendedTwitterProfile } from '../utils/customProfiles';
+
+export const handleTwitterAuth = async (twitterId: string, profile?: ExtendedTwitterProfile): Promise<APIResponse> => {
+
+}
