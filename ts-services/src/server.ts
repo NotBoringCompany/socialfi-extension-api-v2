74631aa25f2d76b6e75b1c546f1bb405d9e90c1d
suwandre <suwandresukijat@gmail.com>
2024-06-15 14:32:37 +0200

    feat: mongoose and express session init

--- ts-services/src/server.ts
+++ ts-services/src/server.ts
@@ -1,12 +1,32 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import passport from 'passport';
+import cors from 'cors';
+import session from 'express-session';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI!;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors()); // temporarily allowing all cors requests
+
+// memory store for session management
+app.use(session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(port, async () => {
     console.log(`Server running on port ${port}`);
+
+    await mongoose.connect(mongoUri);
 });
